a261d277a24273093107acd253cc57a4e68ed908
iambalakrishnan <impythonprogrammer@gmail.com>
2022-01-21 22:37:22 +0530

    added Jinja template and json connectivity

--- flaskcards.py
+++ flaskcards.py
@@ -1,21 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
+from model import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome to my first Flask app!'
+    return render_template('welcome.html',
+                           message='Here is a message from view.')
 
-@app.route('/date')
-def date():
-    return 'This page was served at  date app ' + str(datetime.now())
+@app.route('/card')
+def card_view():
+    card = db[0]
+    return render_template('card.html', card=card)
 
-#add a page that shows how many times the page has been visited
-counter = 0
 
-@app.route('/visits')
-def count_views():
-    global counter #defined outside of the function so it can be used in the function
-    counter += 1
-    return 'This page has been visited ' + str(counter) + ' times.'
+
+# @app.route('/date')
+# def date():
+#     return 'This page was served at  date app ' + str(datetime.now())
+#
+# #add a page that shows how many times the page has been visited
+# counter = 0
+#
+# @app.route('/visits')
+# def count_views():
+#     global counter #defined outside of the function so it can be used in the function
+#     counter += 1
+#     return 'This page has been visited ' + str(counter) + ' times.'
---
+++ model.py
@@ -0,0 +1,18 @@
+"""
+model.py
+---------
+Implements the model for our website by simulating a database.
+
+Note: Although this is nice as a simple, don't do this in a real-world production settings.
+Having a global object for application data is asking for trouble.
+Instead, use a real database layer, like https://flask-sqlalchemy.palletsprojects.com/en/2.x/
+"""
+
+import json
+
+
+def load_db():
+    with open("flashcards_db.json") as f:
+        return json.load(f)
+
+db = load_db()
